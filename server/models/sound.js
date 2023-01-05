8cd5c02be57a25a038a6ca0bbd29bcac54329463
Julia Lipniewicz <julialipniewicz@yahoo.de>
2023-01-05 21:20:09 +0100

    1st-day, sound module

---
+++ server/models/sound.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  artist: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  category: {
+    type: String,
+    enum: ['Song', 'Sample', 'Podcast'],
+    required: true
+  },
+  genre: {
+    type: String,
+    trim: true
+  },
+  format: {
+    type: String,
+    enum: ['MP3', 'WAV', 'AIFF'],
+    required: true
+  },
+  image: {
+    type: String
+  }
+});
+
+const Sound = mongoose.model('Sound', schema);
+
+module.exports = Sound;
