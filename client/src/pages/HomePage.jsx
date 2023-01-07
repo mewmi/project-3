8d7fb45616ed63d4e97ed9e73b0e04e789999ed7
Julia Lipniewicz <julialipniewicz@yahoo.de>
2023-01-07 16:56:35 +0100

    2nd day- UploadPage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { soundLoadRandom } from "../services/sounds";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  // write code for Randomized Track of the day //
+  return (
+    <div>
+      <p>Welcome to EchoSound</p>
+      <p>
+        <i>The application which provides you with sound of all sorts</i>
+      </p>
+    </div>
+  );
 };
 
 export default HomePage;
--- server/models/sound.js
+++ server/models/sound.js
@@ -2,35 +2,40 @@
 
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  artist: {
-    type: String,
-    required: true,
-    trim: true
+const schema = new mongoose.Schema(
+  {
+    artist: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    category: {
+      type: String,
+      enum: ['Song', 'Sample', 'Podcast'],
+      required: true
+    },
+    genre: {
+      type: String,
+      trim: true
+    },
+    format: {
+      type: String,
+      enum: ['MP3', 'WAV', 'AIFF'],
+      required: true
+    },
+    image: {
+      type: String
+    }
   },
-  title: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  category: {
-    type: String,
-    enum: ['Song', 'Sample', 'Podcast'],
-    required: true
-  },
-  genre: {
-    type: String,
-    trim: true
-  },
-  format: {
-    type: String,
-    enum: ['MP3', 'WAV', 'AIFF'],
-    required: true
-  },
-  image: {
-    type: String
+  {
+    timestamp: true
   }
-});
+);
 
 const Sound = mongoose.model('Sound', schema);
 
