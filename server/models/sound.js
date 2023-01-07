c23bbcda15e82c75e997ec5b5d99c87750db761e
Julia Lipniewicz <julialipniewicz@yahoo.de>
2023-01-07 17:05:24 +0100

    merging problem solved

--- server/models/sound.js
+++ server/models/sound.js
@@ -2,11 +2,35 @@
 
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  artist: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
+const schema = new mongoose.Schema(
+  {
+    artist: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    category: {
+      type: String,
+      enum: ['Song', 'Sample', 'Podcast'],
+      required: true
+    },
+    genre: {
+      type: String,
+      trim: true
+    },
+    format: {
+      type: String,
+      enum: ['MP3', 'WAV', 'AIFF'],
+      required: true
+    },
+    image: {
+      type: String
+    }
   },
   {
     timestamp: true
